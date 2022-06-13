ae66965ab87e9e96685847b972bc53060367e977
Takaaki Nakama <takaaki.nakama@plaid.co.jp>
2022-06-14 00:34:40 +0900

    add

--- main.go
+++ main.go
@@ -1,27 +1,21 @@
-// main.go
+package main
 
 import (
-  "github.com/hashicorp/packer-plugin-sdk/plugin"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+
+	googlecompute "github.com/hashicorp/packer-plugin-googlecompute/builder/googlecompute"
 )
 
-// Assume this implements the packer.Builder interface
-type ExampleBuilder struct{}
-
-// Assume this implements the packer.PostProcessor interface
-type FooPostProcessor struct{}
-
-// Assume this implements the packer.Provisioner interface
-type BarProvisioner struct{}
-
 func main() {
-    pps := plugin.NewSet()
-    pps.RegisterBuilder("example", new(ExampleBuilder))
-    pps.RegisterBuilder(plugin.DEFAULT_NAME, new(AnotherBuilder))
-    pps.RegisterPostProcessor("foo", new(FooPostProcessor))
-    pps.RegisterProvisioner("bar", new(BarProvisioner))
-    err := pps.Run()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        os.Exit(1)
-    }
+	pps := plugin.NewSet()
+	pps.RegisterBuilder(plugin.DEFAULT_NAME, new(googlecompute.Builder))
+	err := pps.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
+
