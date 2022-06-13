fbe87a8ec3d3ee1cdc1b6d28b2515ef59c0262e9
Takaaki Nakama <takaaki.nakama@plaid.co.jp>
2022-06-14 00:51:42 +0900

    add

---
+++ builder/artifact.go
@@ -0,0 +1,153 @@
+package googlecompute
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	registryimage "github.com/hashicorp/packer-plugin-sdk/packer/registry/image"
+)
+
+// Artifact represents a GCE image as the result of a Packer build.
+type Artifact struct {
+	image  *Image
+	driver Driver
+	config *Config
+	// StateData should store data such as GeneratedData
+	// to be shared with post-processors
+	StateData map[string]interface{}
+}
+
+// BuilderId returns the builder Id.
+func (*Artifact) BuilderId() string {
+	return BuilderId
+}
+
+// Destroy destroys the GCE image represented by the artifact.
+func (a *Artifact) Destroy() error {
+	log.Printf("Destroying image: %s", a.image.Name)
+	errCh := a.driver.DeleteImage(a.image.Name)
+	return <-errCh
+}
+
+// Files returns the files represented by the artifact.
+func (*Artifact) Files() []string {
+	return nil
+}
+
+// Id returns the GCE image name.
+func (a *Artifact) Id() string {
+	return a.image.Name
+}
+
+// String returns the string representation of the artifact.
+func (a *Artifact) String() string {
+	return fmt.Sprintf("A disk image was created: %v", a.image.Name)
+}
+
+func (a *Artifact) State(name string) interface{} {
+	if name == registryimage.ArtifactStateURI {
+		img, _ := registryimage.FromArtifact(a,
+			registryimage.WithID(a.Id()),
+			registryimage.WithProvider("gce"),
+			registryimage.WithRegion(a.config.Zone),
+		)
+
+		labels := map[string]string{
+			"self_link":    a.image.SelfLink,
+			"project_id":   a.image.ProjectId,
+			"disk_size_gb": strconv.FormatInt(a.image.SizeGb, 10),
+			"machine_type": a.config.MachineType,
+			"licenses":     strings.Join(a.image.Licenses, ","),
+		}
+
+		// Set source image and/or family as labels
+		if a.config.SourceImage != "" {
+			labels["source_image"] = a.config.SourceImage
+		}
+		if a.config.SourceImageFamily != "" {
+			labels["source_image_family"] = a.config.SourceImageFamily
+		}
+
+		// Set PARtifact's source image name from state; this is set regardless
+		// of whether image or image family were used:
+		data, ok := a.StateData["generated_data"].(map[string]interface{})
+		if ok {
+			img.SourceImageID = data["SourceImageName"].(string)
+		}
+
+		if len(a.config.SourceImageProjectId) > 0 {
+			labels["source_image_project_ids"] = strings.Join(a.config.SourceImageProjectId, ",")
+		}
+
+		for k, v := range a.image.Labels {
+			labels["tags"] = labels["tags"] + fmt.Sprintf("%s:%s", k, v)
+		}
+
+		img.Labels = labels
+		return img
+	}
+
+	if name == registryimage.ArtifactStateURI {
+		img, _ := registryimage.FromArtifact(a,
+			registryimage.WithID(a.Id()),
+			registryimage.WithProvider("gce"),
+			registryimage.WithRegion(a.config.Zone),
+		)
+
+		labels := map[string]string{
+			"self_link":    a.image.SelfLink,
+			"project_id":   a.image.ProjectId,
+			"disk_size_gb": strconv.FormatInt(a.image.SizeGb, 10),
+			"machine_type": a.config.MachineType,
+			"licenses":     strings.Join(a.image.Licenses, ","),
+		}
+
+		// Set source image and/or family as labels
+		if a.config.SourceImage != "" {
+			labels["source_image"] = a.config.SourceImage
+		}
+		if a.config.SourceImageFamily != "" {
+			labels["source_image_family"] = a.config.SourceImageFamily
+		}
+
+		// Set PARtifact's source image name from state; this is set regardless
+		// of whether image or image family were used:
+		data, ok := a.StateData["generated_data"].(map[string]interface{})
+		if ok {
+			img.SourceImageID = data["SourceImageName"].(string)
+		}
+
+		if len(a.config.SourceImageProjectId) > 0 {
+			labels["source_image_project_ids"] = strings.Join(a.config.SourceImageProjectId, ",")
+		}
+
+		for k, v := range a.image.Labels {
+			labels["tags"] = labels["tags"] + fmt.Sprintf("%s:%s", k, v)
+		}
+
+		img.Labels = labels
+		return img
+	}
+
+	switch name {
+	case "ImageName":
+		return a.image.Name
+	case "ImageSizeGb":
+		return a.image.SizeGb
+	case "AccountFilePath":
+		return a.config.AccountFile
+	case "ProjectId":
+		return a.config.ProjectId
+	case "BuildZone":
+		return a.config.Zone
+	}
+
+	if _, ok := a.StateData[name]; ok {
+		return a.StateData[name]
+	}
+
+	return nil
+
+}
---
+++ builder/artifact_test.go
@@ -0,0 +1,71 @@
+package googlecompute
+
+import (
+	"testing"
+
+	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
+	registryimage "github.com/hashicorp/packer-plugin-sdk/packer/registry/image"
+	"github.com/mitchellh/mapstructure"
+)
+
+func TestArtifact_impl(t *testing.T) {
+	var _ packersdk.Artifact = new(Artifact)
+}
+
+func TestArtifactState_StateData(t *testing.T) {
+	expectedData := "this is the data"
+	artifact := &Artifact{
+		StateData: map[string]interface{}{"state_data": expectedData},
+	}
+
+	// Valid state
+	result := artifact.State("state_data")
+	if result != expectedData {
+		t.Fatalf("Bad: State data was %s instead of %s", result, expectedData)
+	}
+
+	// Invalid state
+	result = artifact.State("invalid_key")
+	if result != nil {
+		t.Fatalf("Bad: State should be nil for invalid state data name")
+	}
+
+	// Nil StateData should not fail and should return nil
+	artifact = &Artifact{}
+	result = artifact.State("key")
+	if result != nil {
+		t.Fatalf("Bad: State should be nil for nil StateData")
+	}
+}
+
+func TestArtifactState_RegistryImageMetadata(t *testing.T) {
+	artifact := &Artifact{
+		config: &Config{Zone: "us1"},
+		image:  &Image{Name: "test-image", ProjectId: "5678"},
+	}
+
+	// Valid state
+	result := artifact.State(registryimage.ArtifactStateURI)
+	if result == nil {
+		t.Fatalf("Bad: HCP Packer registry image data was nil")
+	}
+
+	var image registryimage.Image
+	err := mapstructure.Decode(result, &image)
+	if err != nil {
+		t.Errorf("Bad: unexpected error when trying to decode state into registryimage.Image %v", err)
+	}
+
+	if image.ImageID != artifact.image.Name {
+		t.Errorf("Bad: unexpected value for ImageID %q, expected %q", image.ImageID, artifact.image.Name)
+	}
+
+	if image.ProviderRegion != artifact.State("BuildZone").(string) {
+		t.Errorf("Bad: unexpected value for ImageID %q, expected %q", image.ProviderRegion, artifact.State("BuildZone").(string))
+	}
+
+	if image.Labels["project_id"] != artifact.image.ProjectId {
+		t.Errorf("Bad: unexpected value for Labels %q, expected %q", image.Labels["project_id"], artifact.image.ProjectId)
+	}
+
+}
